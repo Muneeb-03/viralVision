f81d6f5c2e3a419af051342123f7e4dc46e4c8e1
Muneeb <97738200+Muneeb-03@users.noreply.github.com>
2024-11-04 04:23:54 +0500

    Pricing

--- src/app/components/Payments.js
+++ src/app/components/Payments.js
@@ -130,158 +130,178 @@ export const PaymentSection = () => {
     });
 
     return (
-        <Box id="pricing" sx={{ textAlign: 'center', mt: '75px', px: { xs: '15px', sm: '30px', md: '75px' },}}>
+      <Box
+        id="pricing"
+        sx={{
+          textAlign: "center",
+          mt: "75px",
+          px: { xs: "15px", sm: "30px", md: "75px" },
+        }}
+      >
         <Typography
-            variant="h4"
-            component="h2"
-            sx={{
+          variant="h4"
+          component="h2"
+          sx={{
+            fontFamily: "Cal Sans",
+            fontSize: { xs: "44px", sm: "44px", md: "56px" },
+            fontWeight: 600,
+            lineHeight: "68.39px",
+            letterSpacing: { xs: "0.88px", sm: "0.88px", md: "1.12px" },
+            textTransform: "capitalize",
+            color: "#050623",
+          }}
+        >
+          Transparent{" "}
+          <span
+            style={{
+              background:
+                "linear-gradient(102deg, #FF6F91 3.29%, #8B4CFC 100%)",
+              WebkitBackgroundClip: "text",
+              WebkitTextFillColor: "transparent",
               fontFamily: "Cal Sans",
-              fontSize: "56px",
-              fontWeight: 600,
-              lineHeight: "68.39px",
-              letterSpacing: "1.12px",
-              textTransform: "capitalize",
-              color: "#050623",
             }}
           >
-            Transparent{" "}
-            <span
-              style={{
-                background:
-                  "linear-gradient(102deg, #FF6F91 3.29%, #8B4CFC 100%)",
-                WebkitBackgroundClip: "text",
-                WebkitTextFillColor: "transparent",
-                fontFamily: "Cal Sans",
-              }}
-            >
-              Pricing
-            </span>
+            Pricing
+          </span>
         </Typography>
-        <Typography 
-          variant="subtitle1" 
+        <Typography
+          variant="subtitle1"
           sx={{
-              fontFamily: "Inter",
-              fontSize: "22px",
-              fontWeight: 400,
-              lineHeight: "35.09px",
-              letterSpacing: "1.12px",
-              textTransform: "capitalize",
-              color: "#637381",
-              mt: '25px',
-              mb: '50px'
-            }}>
-          Choose Flexible Pricing Tailored To Your Needs, With No Hidden Fees—Just Impactful, Results-Driven Videos!
+            fontFamily: "Inter",
+            fontSize: { xs: "18.05px", sm: "18.05px", md: "22px" },
+            fontWeight: { xs: 500, sm: 500, md: 400 },
+            lineHeight: { xs: "34.897px", sm: "34.897px", md: "35.09px" },
+            opacity:{ xs: 0.7, sm: 0.7, md: 1 },
+            letterSpacing: "1.12px",
+            textTransform: "capitalize",
+            color: "#637381",
+            mt: "25px",
+            mb: "50px",
+          }}
+        >
+          Choose Flexible Pricing Tailored To Your Needs, With No Hidden
+          Fees—Just Impactful, Results-Driven Videos!
         </Typography>
 
         <Box
-            {...handlers}
-            sx={{
-                display: 'flex', 
-                flexDirection: {
-                  xs: 'column',
-                  sm: 'row',   
-                },
-                justifyContent: {
-                  xs: 'center',
-                  sm: 'center',
-                  md: 'flex-start', 
-                },
-                gap: 3,
-                mb: 4,
-            }}
+          {...handlers}
+          sx={{
+            display: "flex",
+            flexDirection: {
+              xs: "column",
+              sm: "row",
+            },
+            justifyContent: {
+              xs: "center",
+              sm: "center",
+              md: "flex-start",
+            },
+            gap: 3,
+            mb: 4,
+          }}
         >
-            {displayedPlans.map((plan, index) => (
-              <Box
-                key={plan.title}
+          {displayedPlans.map((plan, index) => (
+            <Box
+              key={plan.title}
+              sx={{
+                background: plan.backgroundColor,
+                borderRadius: "24px",
+                width: "100%",
+                maxWidth: "399.14px",
+                height: "670.38px",
+                boxShadow: plan.boxShadow || "none",
+                textAlign: "left",
+                padding: "42.16px 25.3px",
+                color: plan.actionColor,
+                fontFamily: "Poppins",
+              }}
+            >
+              <Typography
                 sx={{
-                  background: plan.backgroundColor,
-                  borderRadius: '24px',
-                  width: '100%',
-                  maxWidth: '399.14px',
-                  height: '670.38px',
-                  boxShadow: plan.boxShadow || 'none',
-                  textAlign: 'left',
-                  padding: '42.16px 25.3px',
-                  color: plan.actionColor,
-                  fontFamily: 'Poppins',
+                  fontFamily: "Poppins",
+                  fontSize: "23.19px",
+                  fontWeight: 700,
+                  lineHeight: "34.78px",
                 }}
               >
-                <Typography
-                  sx={{
-                    fontFamily: 'Poppins',
-                    fontSize: '23.19px',
-                    fontWeight: 700,
-                    lineHeight: '34.78px',
-                  }}
-                >
-                  {plan.title}
-                </Typography>
-                
-                <Typography
-                  sx={{
-                    fontFamily: 'Poppins',
-                    fontSize: '16.86px',
-                    fontWeight: 400,
-                    lineHeight: '25.3px',
-                  }}
-                >
-                  {plan.contentPieces}
-                </Typography>
-                
-                <Typography
-                  sx={{
-                    fontFamily: 'Poppins',
-                    fontSize: '59.03px',
-                    fontWeight: 600,
-                    lineHeight: '88.54px',
-                    my: 2
-                  }}
-                >
-                  {plan.price}
-                </Typography>
-                
-                <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                  <Link href="#contactus">
-                    {plan.buttonVariant === 'contained' ? (
-                      <Image src={InquireFilled} alt="Inquire"/>
-                    ) : (
-                      <Image src={Inquire} alt="Inquire"/>
-                    )}
-                  </Link>
-                </Box>
-                
+                {plan.title}
+              </Typography>
 
-                <Box sx={{ mt: 3 }}>
-                  {plan.features.map((feature, index) => (
-                    <Box
-                      key={index}
-                      sx={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        mb: 2,
-                        color: plan.actionColor,
-                      }}
-                    >
-                      <FeatureIcon feature={feature} planColor={plan.actionColor} />
-                      <Typography sx={{fontFamily: 'Poppins',}}>
-                        {feature.replace('No ', '')}
-                      </Typography>
-                    </Box>
-                  ))}
-                </Box>
+              <Typography
+                sx={{
+                  fontFamily: "Poppins",
+                  fontSize: "16.86px",
+                  fontWeight: 400,
+                  lineHeight: "25.3px",
+                }}
+              >
+                {plan.contentPieces}
+              </Typography>
+
+              <Typography
+                sx={{
+                  fontFamily: "Poppins",
+                  fontSize: "59.03px",
+                  fontWeight: 600,
+                  lineHeight: "88.54px",
+                  my: 2,
+                }}
+              >
+                {plan.price}
+              </Typography>
+
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Link href="#contactus">
+                  {plan.buttonVariant === "contained" ? (
+                    <Image src={InquireFilled} alt="Inquire" />
+                  ) : (
+                    <Image src={Inquire} alt="Inquire" />
+                  )}
+                </Link>
               </Box>
-            ))}
+
+              <Box sx={{ mt: 3 }}>
+                {plan.features.map((feature, index) => (
+                  <Box
+                    key={index}
+                    sx={{
+                      display: "flex",
+                      alignItems: "center",
+                      mb: 2,
+                      color: plan.actionColor,
+                    }}
+                  >
+                    <FeatureIcon
+                      feature={feature}
+                      planColor={plan.actionColor}
+                    />
+                    <Typography sx={{ fontFamily: "Poppins" }}>
+                      {feature.replace("No ", "")}
+                    </Typography>
+                  </Box>
+                ))}
+              </Box>
+            </Box>
+          ))}
         </Box>
 
-        {!isMdUp && <Box sx={{ display: 'flex', justifyContent: 'center', gap: '5px' }}>
+        {!isMdUp && (
+          <Box sx={{ display: "flex", justifyContent: "center", gap: "5px" }}>
             {Array.from({ length: pageCount }, (_, index) => (
-            <DotButton
+              <DotButton
                 key={index}
                 active={index === currentPage}
                 onClick={() => handlePageChange(index)}
-            />
+              />
             ))}
-        </Box>}
+          </Box>
+        )}
       </Box>
-    )
+    );
 }
