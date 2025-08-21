f46c6b127184e83b6e6d2fdf4262902f7f2a49b3
Muneeb Ahmad <muneeb.ahmed@tkxel.io>
2025-08-21 15:43:22 +0500

    Updated Config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false, 
-    webpack: (config) => {
-      config.resolve.fallback = { fs: false, path: false };
-      return config;
-    },
-}
-  
+  reactStrictMode: false,
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, path: false };
+    return config;
+  },
+};
+
 export default nextConfig;
